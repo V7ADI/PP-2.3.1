d9a44c3aaa9ea35ae991151a7d8f87ab6497a914
V7ADI <therion.it@gmail.com>
2022-02-27 14:22:13 +0400

    231
    231

---
+++ src/main/java/spring/mvc/hibernate/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package spring.mvc.hibernate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.mvc.hibernate.dao.UserDAO;
+import spring.mvc.hibernate.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> listUsers() {
+        return userDAO.listUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDAO.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public User getById(int id) {
+        return userDAO.getById(id);
+    }
+
+    @Override
+    @Transactional
+    public void update(User user) {
+        userDAO.update(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+        userDAO.delete(id);
+    }
+}
